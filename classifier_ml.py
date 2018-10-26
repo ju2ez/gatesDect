8eed2cd133f852be23558b358f9892b9d58957ac
ju2ez <JulianHatzky@googlemail.com>
2018-10-26 16:39:14 +0200

    Optimized the classifier

--- classifier_ml.py
+++ classifier_ml.py
@@ -22,6 +22,7 @@ for s in ("test","train"):
 df.columns = ['article' , 'author']
 feature_set = df.loc[:,:].values
 
+
 ###Step 2: Data split 
 from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = train_test_split (df['article'],df['author'] ,test_size=0.2, random_state=42)
@@ -29,11 +30,21 @@ features_train, features_test, labels_train, labels_test = train_test_split (df[
 ### Step 3: tf-idf ( Term frequency - inversse document frequency) processing
 
 from sklearn.feature_extraction.text import TfidfVectorizer as TFIV
-tfv = TFIV(  max_features=None, 
+
+get_stop_words= TFIV(  max_features=None, 
         strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
         use_idf=1,smooth_idf=1,sublinear_tf=1,
         stop_words = 'english')
 
+my_stop_words = set(get_stop_words.get_stop_words())
+my_stop_words.add("link")
+
+tfv = TFIV(  max_features=None, 
+        strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
+        use_idf=1,smooth_idf=1,sublinear_tf=1,
+        stop_words = my_stop_words)
+
+my_stop_words = tfv.get_stop_words()
 tfid_train=tfv.fit_transform(features_train)
 tfid_test =tfv.transform(features_test)
 
@@ -51,8 +62,10 @@ clf.fit(tfid_train,labels_train)
 #Step 4 : cross validation or other type of validation to check your accurarcy 
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
+scores = cross_val_score(clf, tfid_test, labels_test, cv=5)
 scoresF1 = cross_val_score ( clf, tfid_test, labels_test, cv = 5, scoring = 'f1_macro' )
 print ("F1 scores: ",scoresF1)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
  
 
 ###Step 5 : test with predictions !!!!
---
+++ create_more_test_data/scrapeMediumMelinda.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import re 
+
+hdr = {'User-Agent': 'Mozilla/5.0'}
+
+articleCounter=1
+with open("websitesUrlMelinda") as file:
+    for url in file:
+        try:
+            print('Scraping from URL', url)
+            req = urllib.request.Request(url,headers=hdr)
+            html_page =  urllib.request.urlopen(req).read()
+            soup = BeautifulSoup(html_page, 'lxml')
+            myDivs = soup.findAll("div", {"class": "article"} )
+            myArticle = soup.findAll ("div", {"class":"section-content"})
+            article = (myArticle[0].find_all('p'))
+            articleFiltered=""
+            for element in article:
+                for word in element:
+                    myStr = str(word)
+                    if "<" in myStr:
+                        word="LINK "   
+                    articleFiltered+=str(word)
+            if len(articleFiltered)>10:
+                #Use regular expressions to only include words.
+                articleFiltered = re.sub("[^a-zA-Z]"," ", articleFiltered)
+              #  articleFiltered=re.sub("[ ,.-;:/\"]", " ", articleFiltered)
+                #Convert words to lower case and split them into sperate words.
+                articleFiltered = articleFiltered.lower().split()
+                with open("other_articles_for_testing/article_melinda_test"+str(articleCounter)+"_melinda", 'w+') as f:
+                    for item in articleFiltered:
+                        f.write("%s " %item)
+                articleCounter+=1
+        except (KeyboardInterrupt, SystemExit):
+             raise
+      #  except:
+       #     print ("Parsing Error")
